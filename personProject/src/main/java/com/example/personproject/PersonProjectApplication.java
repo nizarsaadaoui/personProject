7c8d55fa53bd3900bd6a1cb842d7d4052b43077e
nizar <nizar.saadaoui.ars@gmail.com>
2023-06-27 22:53:58 +0100

    add bean mapper

--- personProject/src/main/java/com/example/personproject/PersonProjectApplication.java
+++ personProject/src/main/java/com/example/personproject/PersonProjectApplication.java
@@ -1,13 +1,22 @@
-package com.example.personProject;
+package com.example.personproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
 
 @SpringBootApplication
 public class PersonProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonProjectApplication.class, args);
+	}
+
+	@Bean
+	public Mapper mapper() {
+		return DozerBeanMapperBuilder.buildDefault();
 	}
 
 }
